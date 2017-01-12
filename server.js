4fd32ec1b4a8faaba8d4059dd05b283e1925b120
Maximiliano Pozzi <maximilianopozzidp@gmail.com>
2017-01-11 22:01:24 -0300

    Added projects view

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
